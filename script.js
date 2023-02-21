b90ce030725afe6cc5e90df433329c5df130c294
Miguel Bertol <miguelbertolcodes@gmail.com>
2023-02-21 13:06:09 -0300

    feat: import the json file as an object

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Hello world!");
+const requestURL = "./recipes.json";
+const request = new Request(requestURL);
+const response = await fetch(request);
+const recipes = await response.json(); //Recipes JSON object
