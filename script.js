b0c2056fdf8b16419e814a76439e9e44db106bbb
Miguel Bertol <miguelbertolcodes@gmail.com>
2023-02-21 13:45:55 -0300

    comment: add todos

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const requestURL = "./recipes.json";
 const request = new Request(requestURL);
 const response = await fetch(request);
 const recipes = await response.json(); //Recipes JSON object
+
+//TODO: Function to show the recipes from the JSON object
+
+//TODO: Show the recipes titles
+
+//TODO: Show the recipes serves
+
+//TODO: Add the recipes serves slider
+
+//TODO: Add the reset serves button
+
+//TODO: Show the recipes Ingredients
+
+//TODO: Show the recipes Instructions
+
+//TODO: Function to change the ingredients amount according to the serves slider
+
+//TODO: Functio to reset ther serves slider to deafult according to the recipe
